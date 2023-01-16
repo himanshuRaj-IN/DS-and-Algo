2b97df4516fddc0f894539fa237c76c5a70c78d1
Himanshu Raj <himanshuraj9194@gmail.com>
2023-01-16 20:10:31 +0530

    added cp folder

---
+++ BasicBitOperation.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // Testing various Bit Manupulation Operator
+    // 1. Bitwise and < & > 
+    cout << (8 & 11) << endl;
+    // 2. Bitwise Or < | >
+    cout << (8 | 11) << endl;
+    // 3. Bitwise Not < ~ > 
+    cout << (~0) << endl;
+    //4. Bitwise LeftShift Operator < << > 
+    cout << (11<<2) << endl;
+    //5. Bitwise RightShife Operator < >> >
+    cout << (10>>2) << endl;
+     
+    
+   return 0;
+}
---
+++ DataStructures/StackUsingArray.java
@@ -0,0 +1,6 @@
+package DataStructures;
+class StackUsingArray{
+public static void main(String[] args) {
+    System.out.println("Tesing environment");
+}
+}
