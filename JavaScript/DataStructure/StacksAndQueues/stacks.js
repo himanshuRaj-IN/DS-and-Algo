a2816c43e89ebac646b42077d947c4fe218f6948
HIMANSHU RAJ <himanshu.raj@gmai.com>
2023-09-17 16:14:16 +0530

    ArrayImplementationof Stack

---
+++ JavaScript/DataStructure/StacksAndQueues/stacks.js
@@ -0,0 +1,13 @@
+// Array Implementation of Stack
+
+//Stack Array 
+
+let Stack = [];
+
+// ------------ Simple Array with only use of push and pop method 
+
+Stack.push("himashu")
+Stack.push('30393')
+Stack.push('Suresh')
+
+console.log(Stack.pop());
